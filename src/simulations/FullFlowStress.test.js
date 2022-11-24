397d434520c473fd53d6b5a4d2a53f209c80a93c
tiagonline <tiagonline@gmail.com>
2022-11-24 15:52:23 -0300

    ajustes do thresholds test stress

--- src/simulations/FullFlowStress.test.js
+++ src/simulations/FullFlowStress.test.js
@@ -11,6 +11,10 @@ export let options = {
     'http_req_duration': ['p(99)<1500'], // 99% das solicitações devem ser concluídas abaixo de 1,5s
     'logged in successfully': ['p(99)<1500'], // 99% das solicitações devem ser concluídas abaixo de 1,5s
     'http_req_failed': ['rate<0.01'], // erros http devem ser menores que 1%
+
+          // Outro formato de configuração do thresholds
+          // 90% das solicitações devem terminar em 400 ms, 95% em 800 e 99,9% em 2s.
+          //http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000']
   },
 };
 
